ab3fddb7f99c5ab219aeb89e04d996bce1a98fc8
Paul Kinlan <paul.kinlan@gmail.com>
2021-09-28 10:24:54 +0100

    bump

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ interface BuildInfo {
 }
 
 const TMP = tmpdir();
-const DEFAULT_DENO_VERSION = 'v1.14.0';
+const DEFAULT_DENO_VERSION = 'v1.14.1';
 
 // `chmod()` is required for usage with `vercel-dev-runtime` since
 // file mode is not preserved in Vercel deployments from the CLI.
